e925b21c0f1d655da53d313bde4f0690e3fa806d
Micah Wells <micahp0506@gmail.com>
2016-02-23 15:38:28 -0600

    Added link to database

---
+++ models/login.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const BCRYPT_DIFFICULTY = 11;
+
+const UserSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+
+UserSchema.methods.authenticate = function (password, callback) {
+    bcrypt.compare(password, this.password, callback);
+};
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, BCRYPT_DIFFICULTY, (err, hash) => {
+        if (err) throw (err);
+
+        this.password = hash;
+        next();
+    });
+});
+
+module.exports = mongoose.model('Players', UserSchema);
