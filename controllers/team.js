6df00bce9a53d31cdb742e94163f15774c132502
Micah Wells <micahp0506@gmail.com>
2016-02-25 16:28:15 -0600

    Dealing with CBS Sports API is worse than having a tooth pulled

--- controllers/team.js
+++ controllers/team.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const request = require('request');
+
 
 module.exports.index = (req, res) => {
-    console.log("team");
+    const url = 'http://api.cbssports.com/fantasy/sports?version=3.0&response_format=JSON';
+    request.get(url, (err, respnse, body) => {
+    if (err) throw err;
+
+
+    let result = JSON.parse(body);
+    // console.log(result.body.sports[0]);
+    console.log(result.body.sports[0]);
+    res.header('Access-Controll-Allow-Origin', '*');
     res.render('team');
+  });
 };
+
